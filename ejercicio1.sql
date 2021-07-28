e5036ecdb6c582ca0575536f7bbcba1464805ab2
Adrián Pobo <adrianpoboadell@gmail.com>
2021-07-28 12:00:50 +0200

    ejercicio 1 acabado
    pensaba que ya estaba acabado y aun me quedaba la mitad xd. Ya esta acabado

--- ejercicio1.sql
+++ ejercicio1.sql
@@ -32,8 +32,39 @@ from articulos a
 INNER join fabricantes f
 on a.FABRICANTE = f.CODIGO;
 /*1.12*/
+select avg(a.PRECIO), f.CODIGO as 'Fabricante'
+from articulos a
+INNER join fabricantes f
+on a.FABRICANTE = f.CODIGO
+group by f.CODIGO;
+/*1.13*/
 select avg(a.PRECIO), f.NOMBRE as 'Fabricante'
 from articulos a
 INNER join fabricantes f
 on a.FABRICANTE = f.CODIGO
 group by f.NOMBRE;
+/*1.14*/
+SELECT f.NOMBRE
+from fabricantes f
+inner join articulos a
+on a.FABRICANTE = f.CODIGO
+group by f.NOMBRE
+having avg(a.PRECIO) >= 150
+/*1.15*/
+select precio, nombre from articulos
+where PRECIO = (select min(PRECIO) from articulos);
+/*1.16*/
+select a.precio, a.nombre, f.nombre, f.codigo
+from articulos a
+join fabricantes f
+on f.CODIGO = a.fabricante
+where a.precio = (select max(a.precio) from articulos where a.fabricante = f.codigo)
+/*1.17*/
+insert into articulos values (10, 'Altavoz', 70, 2)
+/*1.18*/
+replace into articulos values (8, 'Impresora Laser', 270, 3)
+/*1.19*/
+update articulos set PRECIO = PRECIO * 0.9
+/*1.20*/
+update articulos set PRECIO = PRECIO - 10
+where precio  >= 120
